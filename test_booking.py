b806dbbe21fb103c38286c346d6ba01ba493261a
daeha.ryu <daeha.ryu@samsungds.net>
2024-06-14 11:39:56 +0900

    [feature] add test capacity booking

--- test_booking.py
+++ test_booking.py
@@ -1,5 +1,5 @@
 import unittest
-from datetime import datetime
+from datetime import datetime, timedelta
 
 from booking_scheduler import BookingScheduler
 from schedule import Customer
@@ -32,7 +32,16 @@ class BookingSchedulerTest(unittest.TestCase):
         self.assertEqual("Number of people is over restaurant capacity per hour", str(context.exception))
 
     def test_시간대별_인원제한이_있다_같은_시간대가_다르면_Capacity_차있어도_스케쥴_추가_성공(self):
-        pass
+        schedule = Schedule(ON_HOUR, CAPACITY_PER_H, CUSTOMER)
+        self.booking_sch.add_schedule(schedule)
+
+        diff_time = ON_HOUR + timedelta(hours=1)
+        new_sch = Schedule(diff_time, UNDER_CAPACITY, CUSTOMER)
+        self.booking_sch.add_schedule(new_sch)
+
+        self.assertTrue(self.booking_sch.has_schedule(schedule))
+
+
 
     def test_예약완료시_SMS는_무조건_발송(self):
         pass
