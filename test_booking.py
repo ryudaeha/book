6913b68d8554cc81073f7438e5ceb7ff4ecd247f
daeha.ryu <daeha.ryu@samsungds.net>
2024-06-14 11:29:10 +0900

    [refacotor] update UNDER_CAPACITY, PER_HOUR

--- test_booking.py
+++ test_booking.py
@@ -8,16 +8,18 @@ from schedule import Schedule
 NOT_ON_HOUR = datetime.strptime('2021/03/26 09:05', "%Y/%m/%d %H:%M")
 ON_HOUR = datetime.strptime('2021/03/26 09:00', "%Y/%m/%d %H:%M")
 CUSTOMER = Customer("Fake name", "010-5444-4523")
+UNDER_CAPACITY = 1
+CAPACITY_PER_H = 3
 class BookingSchedulerTest(unittest.TestCase):
 
     def test_예약은_정시에만_가능하다_정시가_아닌경우_예약불가(self):
-        schedule = Schedule(NOT_ON_HOUR, 1, CUSTOMER)
-        booking_sch  = BookingScheduler(3)
+        schedule = Schedule(NOT_ON_HOUR, UNDER_CAPACITY, CUSTOMER)
+        booking_sch  = BookingScheduler(CAPACITY_PER_H)
         with self.assertRaises(ValueError):
             booking_sch.add_schedule(schedule)
     def test_예약은_정시에만_가능하다_정시인_경우_예약가능(self):
-        schedule = Schedule(ON_HOUR, 1, CUSTOMER)
-        booking_sch  = BookingScheduler(3)
+        schedule = Schedule(ON_HOUR, UNDER_CAPACITY, CUSTOMER)
+        booking_sch  = BookingScheduler(CAPACITY_PER_H)
         booking_sch.add_schedule(schedule)
         self.assertTrue(booking_sch.has_schedule(schedule))
     def test_시간대별_인원제한이_있다_같은_시간대에_Capacity_초과할_경우_예외발생(self):
